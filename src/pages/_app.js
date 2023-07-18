6087d9501abda41c702b4148275c2e238f7143c1
Waiyan Min Thant <42428151+waiyanminthant@users.noreply.github.com>
2023-07-18 19:20:21 +0630

    Added Search & Filter by Region

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,61 +1,33 @@
-import { Group, AppShell, Navbar, MantineProvider, MediaQuery, useMantineTheme, Footer, Header, Burger, Text, Mark } from '@mantine/core';
-import { useState } from 'react';
-import NavbarLinks from '../components/navbarlinks';
+import { MantineProvider } from '@mantine/core';
 
 
 export default function App({ Component, pageProps }) {
-  const [opened, setOpened] = useState(false);
-  const theme = useMantineTheme();
 
   return (
     <MantineProvider
       withGlobalStyles
       withNormalizeCSS
       theme={{
-        colorScheme: 'dark',
-        primaryColor: 'violet',
+        colorScheme: 'light',
+        primaryColor: 'blue',
         dateFormat: 'DD/MM/YYYY',
         defaultRadius: 'sm',
-        fontFamily: 'Roboto'
+        fontFamily: 'Roboto',
+        components: {
+          Container: {
+            defaultProps: {
+              sizes: {
+                xs: 540,
+                sm: 720,
+                md: 960,
+                lg: 1140,
+                xl: 1320,
+              },
+            },
+          },
+        }
       }}>
-      <AppShell
-        navbarOffsetBreakpoint="sm"
-        navbar={
-          <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 250, lg: 250 }}>
-            <NavbarLinks />
-          </Navbar>
-        }
-        footer={
-          <Footer height={50} p="md">
-            <Text fz='xs'>Coded by BP_STUDIO</Text>
-          </Footer>
-        }
-        header={
-          <Header height={{ base: 50, md: 70 }} p="md">
-            <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-                <Burger
-                  opened={opened}
-                  onClick={() => setOpened((o) => !o)}
-                  size="sm"
-                  color={theme.colors.gray[6]}
-                  mr="xl"
-                />
-              </MediaQuery>
-              <Group>
-                <Text fz='lg'>NRC Checker</Text>
-                <Text fz='xs' fw='bolder'>
-                  <Mark color='blue' p={5} sx={{borderRadius: 6}}>
-                    BP_STUDIO
-                  </Mark>
-                </Text>
-              </Group>
-            </div>
-          </Header>
-        }
-      >
-        <Component {...pageProps} />
-      </AppShell>
+      <Component {...pageProps} />
     </MantineProvider>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,69 @@
 import Head from 'next/head'
 import data from 'data/nrc.json';
-import { Center, Container, Table, Pagination, Text, Title, Divider } from '@mantine/core';
+import { Center, Container, Table, Pagination, Group, Paper, TextInput, Select, Text, Anchor } from '@mantine/core';
 import { useState, useEffect } from 'react';
+import { IconSearch } from '@tabler/icons-react';
 
 export default function Home() {
 
   const nrcData = data.nrc
+  const codeList = [
+    { label: 'ကချင်', value: '1' },
+    { label: 'ကယား', value: '2' },
+    { label: 'ကရင်', value: '3' },
+    { label: 'ချင်း', value: '4' },
+    { label: 'စစ်ကိုင်း', value: '5' },
+    { label: 'တနင်္သာရီ', value: '6' },
+    { label: 'ပဲခူး', value: '7' },
+    { label: 'မ‌ကွေး', value: '8' },
+    { label: 'မန္တလေး', value: '9' },
+    { label: 'မွန်', value: '10' },
+    { label: 'ရခိုင်', value: '11' },
+    { label: 'ရန်ကုန်', value: '12' },
+    { label: 'ရှမ်း', value: '13' },
+    { label: 'ဧရာဝတီ', value: '14' },
+  ]
 
   const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  const [codeSearch, setCodeSearch] = useState('');
   const [paginatedNRC, setPagniatedNRC] = useState(nrcData);
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(20);
   const [totalPage, setTotalPage] = useState(1);
 
+  function filterNRC(search, codeSearch) {
+    const keyword = search.toLowerCase();
+
+    if (codeSearch) {
+      var codeFiltered = nrcData.filter((item) => item.nrc_code === codeSearch)
+    } else {
+      var codeFiltered = nrcData
+    }
+
+    const filtered = codeFiltered.filter((item) =>
+      item.district_code_mm.includes(keyword) ||
+      item.district_code_en.toLowerCase().includes(keyword) ||
+      item.state_mm.includes(keyword) ||
+      item.state_en.toLowerCase().includes(keyword) ||
+      item.nrc_code.toLowerCase().includes(keyword)
+    )
+    return filtered
+  }
+
   useEffect(() => {
     setIsLoading(true);
-    setTotalPage(Math.ceil(nrcData.length / pageSize))
-    setPagniatedNRC(nrcData.slice((page * pageSize) - pageSize, (page * pageSize)))
+    const filtered = filterNRC(search, codeSearch);
+    setTotalPage(Math.ceil(filtered.length / pageSize))
+    setPagniatedNRC(filtered.slice((page * pageSize) - pageSize, (page * pageSize)))
     setIsLoading(false)
-  }, [page])
+  }, [page, search, codeSearch])
 
-  const rows = paginatedNRC.map((item, index) => {
+  const rows = paginatedNRC.map((item) => {
     var regex = /\(([^()]+)\)/g;
     return (
       <tr key={item.district_code_mm}>
+        <td><Center>{item.nrc_code}</Center></td>
         <td><Center>{item.state_mm}</Center></td>
         <td><Center>{(item.district_code_mm).slice(6)}</Center></td>
         <td><Center>{(item.district_code_en)}</Center></td>
@@ -33,19 +73,28 @@ export default function Home() {
   })
 
   return (
-    <>
+    <Container fluid px={50} py={40}>
       <Head>
         <title>NRC Checker</title>
       </Head>
-      <Container pb={30}>
-        <Center><Title order={2}>Myanmar NRC List</Title></Center>
-      </Container>
-      <Divider pb={20} />
-      <Container fluid>
-        <Table withColumnBorders highlightOnHover verticalSpacing='md'>
+      <Paper withBorder shadow='xl' pb={40} pt={20} px={20}>
+        <Group position='center' pb={20}>
+          <Select
+            data={codeList}
+            onChange={(value) => {setCodeSearch(value); setPage(1)} }
+            placeholder='တိုင်း/ပြည်နယ် ရွေးရန်'
+            clearable
+          />
+          <TextInput
+            placeholder='စာဖြင့်ရှာရန်'
+            icon={<IconSearch size={16} />}
+            onChange={(event) => {setSearch(event.currentTarget.value), setPage(1)}}
+          />
+        </Group>
+        <Table withColumnBorders withBorder highlightOnHover verticalSpacing='md'>
           <thead>
             <tr>
-              <th><Center>No.</Center></th>
+              <th><Center>NRC Code</Center></th>
               <th><Center>State</Center></th>
               <th><Center>Region (မြန်မာ)</Center></th>
               <th><Center>Region Code (English)</Center></th>
@@ -53,24 +102,14 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {paginatedNRC.map((item, index) => {
-              var regex = /\(([^()]+)\)/g;
-
-              return (
-
-                <tr key={index + item.id}>
-                  <td><Center>{((page - 1) * pageSize) + index + 1}</Center></td>
-                  <td><Center>{item.state_mm}</Center></td>
-                  <td><Center>{(item.district_code_mm).slice(6)}</Center></td>
-                  <td><Center>{(item.district_code_en)}</Center></td>
-                  <td><Center>{regex.exec(item.district_code_mm)[1]}</Center></td>
-                </tr>
-              )
-            })}
+            {rows}
           </tbody>
         </Table>
-        <Pagination total={totalPage} position='center' pt={30} defaultValue={page} onChange={setPage} disabled={totalPage < 1} />
-      </Container>
-    </>
+        <Pagination total={totalPage} position='center' pt={30} page={page} onChange={setPage} disabled={totalPage < 1} />
+      </Paper>
+      <Center py={20}>
+        <Text fz={12}>Coded by <Anchor href={'https://github.com/waiyanminthant'} target='_blank'>Waiyan Min Thant</Anchor></Text>
+      </Center>
+    </Container>
   )
 }
